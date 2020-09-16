25bcbe8094ee746c0f6263ba3e77fa422b7a7b86
Tokki0401 <gyuhun95@gmail.com>
2020-09-16 16:33:22 -0700

    AboutMe component format/structure complete. Need to work on animation css

--- client/src/components/AboutMe.jsx
+++ client/src/components/AboutMe.jsx
@@ -6,12 +6,16 @@ const AboutMe = (props) => {
 
   return (
     <section className='about-me'>
-      <div className='container'>
-        <div>
-          <img src={Profile} alt="Profile Picture" />
+      <div className='title'>About Me</div>
+      <div className='about-me-container'>
+        <div className='picture'>
+          <img src={Profile} alt='Profile Picture' />
         </div>
-        <div>
-          <p>Hi</p>
+        <div className='detail'>
+          <p>I'm a software engineer currently living in Irvine, CA and looking for my next role.</p>
+          <p>I have experience in cutting-edge frameworks and technologies such as React and Node. I'm always open to take on challenges and learn new technologies. </p>
+          <p>When I'm not coding, I enjoy playing video games and watching K-drama!
+          </p>
         </div>
       </div>
     </section>
